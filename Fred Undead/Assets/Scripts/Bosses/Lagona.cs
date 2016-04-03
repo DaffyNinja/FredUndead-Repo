fcb6ea67d38846d6db020d9b1da3d5f7bffc19f6
Marcus <mdaygmdesigner@gmail.com>
2016-04-03 10:47:53 +1000

    Made placeholder texture folder
    Made lagona boss
    
    Made lagona boss shoot squid

---
+++ Fred Undead/Assets/Scripts/Bosses/Lagona.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lagona : MonoBehaviour
+{
+
+    [Header("Movement")]
+    public float speed;
+
+    [Header("Combat")]
+    public GameObject squidObj;
+    public Transform squidShootPos;
+
+    public float shootTime;
+    //  public float pauseTime;
+    float timer;
+
+    bool startShooting;
+
+    [Header("Health")]
+    public int healthPoints;
+
+
+    GameObject hitHand;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        hitHand = transform.GetChild(0).gameObject;
+        squidShootPos = transform.GetChild(1);
+
+        startShooting = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (startShooting)
+        {
+            timer += Time.deltaTime;
+
+            Instantiate(squidObj, squidShootPos.position, Quaternion.identity);
+
+            startShooting = false;
+        }
+        else
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= shootTime)
+            {
+                timer = 0;
+
+                startShooting = true;
+            }
+
+        }
+
+
+
+    }
+}
---
+++ Fred Undead/Assets/Scripts/Bosses/SquidBullet.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquidBullet : MonoBehaviour
+{
+
+    public int squidDamage;
+    public float speed;
+
+    Rigidbody2D rig2D;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        rig2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveQauntity = new Vector2(-speed, 0);
+        rig2D.velocity = new Vector2(moveQauntity.x, rig2D.velocity.y);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            // print("Player");
+
+            col.gameObject.GetComponent<PlayerMove>().healthPoints -= squidDamage;
+
+        }
+
+        if (col.gameObject.tag != "Enemy")
+        {
+            Destroy(this.gameObject);
+        }
+
+       
+    }
+}
