f57c5ddd7f0d2abb79530f235bc37c0532b46f70
Marcus <mdaygmdesigner@gmail.com>
2016-04-04 15:05:34 +1000

    Made level 1 design 2 scene
    Renamed level weapons test to level 1 design 1
    
    Made door script and placed in door texture
    
    Made main camera a prefab

---
+++ Fred Undead/Assets/Scripts/LevelDoor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelDoor : MonoBehaviour
+{
+
+    public Transform destPos;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                col.gameObject.transform.position = destPos.position;
+            }
+        }
+
+
+
+    }
+}
+
